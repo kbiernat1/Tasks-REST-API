ee00f97249d8594e9afb7c6bd11fae2c570d344f
Kamil Biernat <k.biernat@yahoo.ca>
2022-06-20 17:56:56 +0200

    19.4 exercise '@Scheduled'

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -3,6 +3,7 @@ package com.crud.tasks.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,6 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @OpenAPIDefinition
 @Configuration
+@EnableScheduling
 public class CoreConfiguration {
 
     @Bean
---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private final SimpleEmailService simpleEmailService;
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Scheduled(cron = "0 0 10 * * *")
+    //Scheduled(fixedDelay = 20000)
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        String condition = (size == 1) ? " task" : " tasks";
+        simpleEmailService.send(
+                new Mail(
+                        adminConfig.getAdminMail(),
+                        SUBJECT,
+                        "Currently in database you got: " + size + condition,
+                        null
+                )
+        );
+    }
+}
