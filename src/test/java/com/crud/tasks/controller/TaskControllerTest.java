962bba019db4862383287722e80bb753f1efc994
Kamil Biernat <k.biernat@yahoo.ca>
2022-07-04 16:23:02 +0200

    21.3 exercise TaskController Test: fixed shouldReturnTaskDtoList()

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
